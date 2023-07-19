1e6b5927606dff903129edd1bb9474eb008ceb43
samir <samirgurung927@gmail.com>
2023-07-19 23:15:30 +0545

    Changed the File

--- customerLoginProcess.java
+++ customerLoginProcess.java
@@ -3,8 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
 import java.io.IOException;
 import java.io.PrintWriter;
+import static java.lang.System.out;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -30,17 +36,36 @@ public class customerLoginProcess extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        String email = request.getParameter("");//getting email and password in the form of strig from form
+        String password = request.getParameter("");
         try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet customerLoginProcess</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet customerLoginProcess at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            Class.forName("com.jdbc.mysql.Driver");//connecting Database
+            Connection con =  DriverManager.getConnection("jdbc:mysql://localhost/logdetails","root","");
+           String Verification = "SELECT * FROM Customer";//getting data from Database
+            PreparedStatement statement = con.prepareStatement(Verification);
+            ResultSet rs = statement.executeQuery();
+            int flag = 0;
+            while(rs.next()){
+                
+           
+            if (email.equals("") && password.equals("")) {
+                flag = 1;
+            } else {
+                flag = 0;
+            }
+            }
+            if(flag==1){
+                RequestDispatcher rd = request.getRequestDispatcher("");
+                
+            }else{
+                request.setAttribute("Error Message", "Email or Password is Incorrect");
+                RequestDispatcher rd = request.getRequestDispatcher("");
+                rd.include(request, response);
+            }
+            con.close();
+        } catch (Exception e) {
+            out.print(e);
+
         }
     }
 
